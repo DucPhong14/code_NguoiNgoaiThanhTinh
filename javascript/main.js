5aa7e9f91ed9b6aa610444be782c9314c5cb20ae
DucPhong14 <phonghd123456789@gmail.com>
2021-12-07 21:09:28 +0700

    lan-dau

---
+++ javascript/main.js
@@ -0,0 +1,13 @@
+let Determine = document.getElementById("Determine")
+
+Determine.addEventListener("click", onclick);
+
+function onclick(){
+    var question__1 = document.getElementById("question__1");
+    var answer1 = question__1.options[question1.selectedIndex].text;
+    var question__2 = document.getElementById("question__2");
+    var answer2 = question__2.options[question1.selectedIndex].text;
+    var question__3 = document.getElementById("question__3");
+    var answer3 = question__3.options[question1.selectedIndex].text;
+    
+}
